63ab5a86315e305ae15d361f2140c7a95913c731
viktor-hi <411206167@qq.com>
2019-12-01 01:37:09 +0800

    redis缓存击穿测试修改

--- src/main/java/cn/chen/dao/AccountMapper.java
+++ src/main/java/cn/chen/dao/AccountMapper.java
@@ -1,8 +1,15 @@
 package cn.chen.dao;
 
+import cn.chen.model.Account;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
 /**
  * @author haixin
  * @time 2019/12/1
  */
-public class AccountMapper {
+@Mapper
+public interface AccountMapper {
+    public List<Account> selectAll();
 }
